b7571b827ea08072ba0b007ba56526fb502a3e29
carlstep <carlstep333@me.com>
2022-07-24 10:51:10 +0700

    ListView - basics

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,104 @@ void main() {
       ),
       body: Column(
         children: [
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Information',
+            style: GoogleFonts.poppins(
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
+              color: Colors.black54,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           Container(
-            height: 400,
+            height: 300,
             child: ListView(
+              //scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
               children: [
-                Text('Payment',
-                    style: GoogleFonts.poppins(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.red[600],
-                    )),
+                Text(
+                  'Payment',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.red[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Change',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.blue[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Exchange',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.green[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Payment',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.red[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Change',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.blue[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Exchange',
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.green[600],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
               ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'More Information',
+            style: GoogleFonts.poppins(
+              fontSize: 40,
+              fontWeight: FontWeight.w600,
+              color: Colors.black54,
             ),
           ),
         ],
